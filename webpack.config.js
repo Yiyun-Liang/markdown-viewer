394e035622237e4a46acc59796807c3137a68160
isa <isaliangyiyun@hotmail.com>
2017-01-17 17:56:59 -0500

    setup

---
+++ webpack.config.js
@@ -0,0 +1,46 @@
+var webpack = require('webpack');
+
+// same as running in terminal: webpack ./public/app.js ./public/bundle.js
+module.exports = {
+  entry: [
+    'script!jquery/dist/jquery.min.js',
+    'script!foundation-sites/dist/js/foundation.min.js',
+    './app/app.jsx'
+  ],
+  externals:{
+    jquery: 'jQuery'
+  },
+  plugins:[
+    new webpack.ProvidePlugin({
+      '$': 'jquery',
+      'jQuery': 'jquery'
+    })
+  ],
+  output: {
+    path: __dirname,
+    filename: './public/bundle.js'
+  },
+  resolve: {
+    root: __dirname,
+    alias: {
+      applicationStyles: 'app/styles/main.scss'
+    },
+    extensions: ['', '.js', '.jsx']
+  },
+  module: {
+    loaders: [
+      {
+        // loader to load jsx files
+        loader: 'babel-loader',
+        query: {
+          presets: ['react', 'es2015', 'stage-0']
+        },
+        test: /\.jsx?$/,
+        exclude: /(node_modules|bower_components)/
+      }
+    ]
+  },
+  devtool: 'cheap-module-eval-source-map'
+};
+// with the configs, simply run in terminal: webpack
+// webpack -w, like supervisor for node
